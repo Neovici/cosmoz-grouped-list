1da4712765ccb4221c532667032484eb9fcb00ad
JaySunSyn <jfathi.mail@gmail.com>
2017-09-22 12:03:02 +0200

    Revert "Added highlightItems (multi)"
    This reverts commit 7231132b29625a32a1ad34714c77953e066f2bfc.

--- cosmoz-grouped-list.js
+++ cosmoz-grouped-list.js
@@ -464,13 +464,6 @@
 			}
 		},
 
-		highlightItems(items) {
-			if (!items || !items.length > 0) {
-				return;
-			}
-			items.forEach(i => this.highlightItem(i));
-		},
-
 		highlightItem(item) {
 			var model = this._getModelFromItem(item);
 
