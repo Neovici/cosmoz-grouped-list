0ec614830691c4b2605661d736fc4be7add06314
Cristian Necula <virusz@gmail.com>
2024-02-28 00:40:55 +0200

    feat!: upgrade to lit-virtualizer
    BREAKING CHANGE: drop polymer iron-list
    BREAKING CHANGE: drop updateSize and scrollTarget

--- use-collapsible-items.js
+++ use-collapsible-items.js
@@ -2,35 +2,31 @@ import { useCallback } from '@pionjs/pion';
 import { useWeakState } from './use-weak-state';
 import { isGroup } from './utils';
 
-export const useCollapsibleItems = (callback, foldCallback) => {
+export const useCollapsibleItems = () => {
 	const { setItemState, state, signal } = useWeakState(),
 		toggleFold = useCallback((item, folded) => {
 			if (!isGroup(item)) {
 				return;
 			}
 
-			setItemState(item, state => ({
-				folded: folded !== undefined ? folded : !state.folded
+			setItemState(item, (state) => ({
+				folded: folded !== undefined ? folded : !state.folded,
 			}));
-
-			foldCallback(item);
 		}, []),
 		toggleCollapse = useCallback((item, collapsed) => {
 			if (isGroup(item)) {
 				return;
 			}
 
-			setItemState(item, state => ({
-				expanded: collapsed !== undefined ? !collapsed : !state.expanded
+			setItemState(item, (state) => ({
+				expanded: collapsed !== undefined ? !collapsed : !state.expanded,
 			}));
-
-			callback(item);
 		}, []);
 
 	return {
 		state,
 		signal,
 		toggleFold,
-		toggleCollapse
+		toggleCollapse,
 	};
 };
